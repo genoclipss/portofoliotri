df055135d5bdc04d1eacb57813d8933808fab7c1
genoclipss <genoclipss@gmail.com>
2026-07-04 10:44:22 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -67,7 +67,7 @@ const DATA_PENGALAMAN = {
       instansi: "Pend. Teknik Informatika & Komputer",
       tahun: "2024 — Sekarang",
       deskripsi:
-        "Telah menempuh perguruan tingakt tinggi selama 2 tahun / 4 semester.",
+        "Saya merupakan mahasiswa Program Studi Pendidikan Teknik Informatika dan Komputer (PTIK) di Universitas Negeri Makassar yang saat ini sedang menempuh semester lima. Selama masa perkuliahan, saya aktif mengikuti berbagai kegiatan akademik dan nonakademik.",
       logo: aset("/images/karya/unmm.png"),
       // <-- Isi galeri "Lebih Detail". Kosongkan foto & video ( [] ) jika tidak ingin tombol muncul.
       karya: {
@@ -92,7 +92,7 @@ const DATA_PENGALAMAN = {
       instansi: "Jurusan Multimedia",
       tahun: "2021 — 2024",
       deskripsi:
-        "Telah menempuh pendidikan selama 3 tahun dalam bidang multimedia.",
+        "Saya menempuh pendidikan menengah kejuruan pada jurusan Multimedia, di mana saya , Selama masa pendidikan, saya memperoleh pengalaman dalam pengambilan foto dan video, serta mengembangkan kemampuan mengedit foto dan video",
       logo: aset("/images/karya/smk.png"),
       karya: {
         foto: [
@@ -113,7 +113,7 @@ const DATA_PENGALAMAN = {
       instansi: "Tribun Timur Makassar",
       tahun: "2026 — Sekarang",
       deskripsi:
-        "Selama magang di Tribun Timur, saya berkontribusi dalam bidang redaksi sebagai editor video, pengisi voice over, dan desainer thumbnail. Dari pengalaman tersebut, saya berhasil menghasilkan lebih dari 80 konten digital yang dipublikasikan, sekaligus mengembangkan keterampilan di bidang editing, desain visual, dan komunikasi digital..",
+        "Selama magang di Tribun Timur, saya berkontribusi dalam bidang redaksi sebagai editor video, pengisi voice over, dan desainer thumbnail. Dari pengalaman tersebut, saya berhasil menghasilkan lebih dari 80 konten digital yang dipublikasikan, sekaligus mengembangkan keterampilan di bidang editing, desain visual, dan komunikasi digital.",
       logo: aset("/images/karya/tribun.png"),
       karya: {
         foto: [aset("/images/karya/tribun/1.jpeg")],
@@ -196,7 +196,7 @@ const DATA_PENGALAMAN = {
       instansi: "P.T Fajar TV Makassar",
       tahun: " Juni 2023 — September 2023",
       deskripsi:
-        "Mengedit berita hingga perkiraan cuaca pada program PRIMEZONE, Menjadi Operator pada program live PRIMEZONE, serta turun lapangan untuk meliput berita secara langsung.",
+        "saya terlibat secara langsung dalam proses produksi siaran televisi, mulai dari mengedit konten berita dan segmen prakiraan cuaca pada program PRIMEZONE, menjadi operator saat program PRIMEZONE berlangsung secara live, hingga melakukan peliputan berita di lapangan.",
       logo: aset("/images/karya/fajar.jpg"),
       karya: {
         foto: [
@@ -228,7 +228,7 @@ const DATA_PENGALAMAN = {
       instansi: "Tiktok & Youtube",
       tahun: "2025 — Sekarang ",
       deskripsi:
-        "Semenjak menjadi clipper, saya telah mengupload 24 video pada Youtube dan Tiktok, total like melebihi 90.00k ",
+        "Salah satu hobi yang saya tekuni adalah membuat konten video sebagai clipper. Hingga saat ini, saya telah mengunggah 24 video di YouTube dan TikTok dengan total perolehan lebih dari 90 ribu likes.",
       logo: aset("/images/karya/tiktok.png"),
       karya: {
         foto: [
@@ -240,11 +240,11 @@ const DATA_PENGALAMAN = {
     },
     
     {
-      judul: "Designer",
+      judul: "Desain Grafis",
       instansi: "Instagram",
       tahun: "2025 — Sekarang",
       deskripsi:
-        "Ketika saya luang dan hilang arah, biasanya saya bermain dengan kreativitas saya di software editing",
+        "Sebagai bentuk pengembangan kreativitas, saya sering mengedit foto pribadi dengan konsep visual bergaya poster dan membagikannya melalui media sosial.",
       logo: aset("/images/karya/ig.png"),
       karya: {
         foto: [
